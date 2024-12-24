8043246c68a07a077f57819564b7a4e31b2060ea
ShahmeerGenesis360 <shahmir@genesis360.co>
2024-12-25 02:04:00 +0500

    fixed pagination

--- src/service/commentService.ts
+++ src/service/commentService.ts
@@ -1,5 +1,6 @@
 import { PaginatedResponse } from "../types/index";
 import { Comment, IComment } from "../models/comment";
+import mongoose from "mongoose";
 
 const CommentService = () => {
   const getChatById = async (id: string): Promise<IComment | null> => {
@@ -12,17 +13,18 @@ const CommentService = () => {
     pageSize: number
   ): Promise<PaginatedResponse<IComment>> => {
     // Get the total number of records
-    const totalRecords = await Comment.countDocuments({ index });
+    const id = new mongoose.Types.ObjectId(index);
+    const totalRecords = await Comment.countDocuments({ indexId: id });
 
     // Calculate total pages
     const totalPages = Math.ceil(totalRecords / pageSize);
 
     // Fetch the paginated data
-    const data = await Comment.find({ index })
+    const data = await Comment.find({indexId: id})
       .sort({ createdAt: -1 })
-      .skip(page * pageSize)
-      .limit(pageSize);
-
+      .skip((page-1) * pageSize)
+      .limit(pageSize)
+      .exec();
     // Return the response with meta and data
     return {
       meta: {
