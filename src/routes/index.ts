48d7bcee8a2001c2da314fcf1cbb5315d2f84b17
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-01-07 04:28:56 +0500

    import adminRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import userRouter from "./userRoute";
 import commentRouter from "./commentRoute";
 import portfolioRouter from "./userPortfolioRoutes";
 import reactionRouter from "./reactionRoute";
+import adminRouter from "./adminRoute";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/users", userRouter);
 router.use("/comment", commentRouter);
 router.use("/portfolio", portfolioRouter);
 router.use("/reaction", reactionRouter);
+router.use("/auth", adminRouter);
 
 export default router;
