47a5fec3789ac0a11209aabb41184ebed6928773
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-02-04 05:09:10 +0500

    symbol added in model

--- src/models/groupCoin.ts
+++ src/models/groupCoin.ts
@@ -30,6 +30,7 @@ export interface IGroupCoin extends Document {
   collectorDetail: Array<ICollectorDetail>;
   feeAmount: string;
   category: string;
+  symbol: string;
 }
 
 // Define the ICoin schema
@@ -52,8 +53,8 @@ const faqSchema = new Schema<IFaq>(
 
 const collectorDetailSchema = new Schema<ICollectorDetail>(
   {
-    collector: { type: String, },
-    weight: { type: Number,},
+    collector: { type: String },
+    weight: { type: Number },
   },
   { _id: false } // Prevent Mongoose from creating a separate _id for each subdocument
 );
@@ -67,11 +68,12 @@ const groupCoinSchema = new Schema<IGroupCoin>(
     description: { type: String, required: true },
     visitCount: { type: Number, default: 0 },
     faq: { type: [faqSchema], required: true },
-    mintKeySecret: {type: String, required: true},
-    mintPublickey: {type: String, required:true},
-    collectorDetail: { type: [collectorDetailSchema],  },
-    feeAmount: {type: String, required: true},
-    category: {type: String, required: true}
+    mintKeySecret: { type: String, required: true },
+    mintPublickey: { type: String, required: true },
+    collectorDetail: { type: [collectorDetailSchema] },
+    feeAmount: { type: String, required: true },
+    category: { type: String, required: true },
+    symbol: { type: String },
   },
   { timestamps: true }
 );
